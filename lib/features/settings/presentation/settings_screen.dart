ec6d8156e0741e99f40b4c22b2ce846bea634802
RarDog <playfirty954@gmail.com>
2026-09-03 17:20:49 +0300

    fix(build): resolve Linux gradle execution and Flutter 3.47 import conflict

--- lib/features/settings/presentation/settings_screen.dart
+++ lib/features/settings/presentation/settings_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' hide appBuildNumber;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_launcher/url_launcher.dart';
